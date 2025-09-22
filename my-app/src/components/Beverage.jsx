6a6918b003ee79276189702f9790d64a1e9f156a
Aira Estur <airaaltisestur@gmail.com>
2025-09-22 16:39:48 +0800

    font-awesome

---
+++ my-app/src/components/Beverage.jsx
@@ -0,0 +1,10 @@
+// src/components/Beverage.jsx
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+export const Beverage = () => (
+  <div>
+    <FontAwesomeIcon icon="check-square" />
+    Your <FontAwesomeIcon icon="coffee" /> is hot and ready!
+  </div>
+);
