8f2202f818cbcd36f8329684143ab3555e07f05d
Aira Estur <airaaltisestur@gmail.com>
2025-10-09 14:34:25 +0800

    concept works

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -4,13 +4,28 @@ import { motion, AnimatePresence } from "framer-motion";
 import { useInView } from "framer-motion";
 import clsx from "clsx";
 
-// Import your images from src/assets/mock-ups
+// Import logos
 import airaIcon from "./assets/icons/airaestur.png";
 import dostIcon from "./assets/icons/dost.png";
 import pricepulseIcon from "./assets/icons/pricepulse.png";
 import figmaIcon from "./assets/icons/figma.png";
+import aiIcon from "./assets/icons/aillustrator.png";
+import csharpIcon from "./assets/icons/csharp.png";
+import htmlIcon from "./assets/icons/html.png";
+import cssIcon from "./assets/icons/css.png";
+import jsIcon from "./assets/icons/js.png";
+import pyIcon from "./assets/icons/python.png";
+
+
+// Import mock-ups
 import dostMock from "./assets/mock-ups/dost.png";
 import pricepulseMock from "./assets/mock-ups/pricepulse.png";
+import amwayMock from "./assets/mock-ups/amway.jpg";  
+import toyrushMock from "./assets/mock-ups/toyrushgame.jpg";  
+import gtaraMock from "./assets/mock-ups/gtara.png";  
+import fruitvendingMock from "./assets/mock-ups/guivendingmachine.png";  
+
+// Import icons
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowRight } from "@fortawesome/free-solid-svg-icons/faArrowRight";
 import { faTrophy } from "@fortawesome/free-solid-svg-icons";
@@ -151,6 +166,84 @@ function App() {
             </div>
           </div>
         </div>
+      </div>
+
+       {/* Concept Works Section */}
+      <div className="section_concept_works">
+        <h2> Concept Works </h2>
+        <div className="section_cards_concept_works">
+          <div className="card_concept_works">
+            <img src={amwayMock} className="concept_works_banner" alt="mock-ups_amway" />
+            <div className="card_concept_works_content">
+              <h3> Amway or the Highway </h3>
+              <p> A board game that blends the classic gameplay mechanics of Monopoly with the dynamic world of Amway products. </p>
+          </div>
+          <div className="card_concept_works_lower"> 
+            <button>
+              Live View <FontAwesomeIcon className="icon_fontawesome" icon={faArrowRight} />
+            </button>
+            <div className="card_concept_works_footer">
+                <p> Technologies used: </p>
+                <img src={figmaIcon} className="icon_logos_technologies" alt="icon_figma" />
+            </div>
+          </div>
+          </div>   
+
+          <div className="card_concept_works">
+            <img src={toyrushMock} className="concept_works_banner" alt="mock-ups_amway" />
+            <div className="card_concept_works_content">
+              <h3> Toy Rush! </h3>
+              <p> A game that will awaken your inner child for it transports you to a world of excitement and wonder! </p>
+          </div>
+          <div className="card_concept_works_lower"> 
+            <button>
+              Live View <FontAwesomeIcon className="icon_fontawesome" icon={faArrowRight} />
+            </button>
+            <div className="card_concept_works_footer">
+                <p> Technologies used: </p>
+                <img src={csharpIcon} className="icon_logos_technologies" alt="icon_figma" />
+                <img src={aiIcon} className="icon_logos_technologies" alt="icon_figma" />
+            </div>
+          </div>
+          </div>  
+
+          <div className="card_concept_works">
+            <img src={gtaraMock} className="concept_works_banner" alt="mock-ups_amway" />
+            <div className="card_concept_works_content">
+              <h3> G-Tara Na! </h3>
+              <p> A Website which checks if the played chord of the guitarist or the user is correct or incorrect. </p>
+          </div>
+          <div className="card_concept_works_lower"> 
+            <button>
+              Live View <FontAwesomeIcon className="icon_fontawesome" icon={faArrowRight} />
+            </button>
+            <div className="card_concept_works_footer">
+                <p> Technologies used: </p>
+                <img src={htmlIcon} className="icon_logos_technologies" alt="icon_figma" />
+                <img src={cssIcon} className="icon_logos_technologies" alt="icon_figma" />
+                <img src={jsIcon} className="icon_logos_technologies" alt="icon_figma" />
+            </div>
+          </div>
+          </div>  
+
+          <div className="card_concept_works">
+            <img src={fruitvendingMock} className="concept_works_banner" alt="mock-ups_amway" />
+            <div className="card_concept_works_content">
+              <h3> Fruit Vending Machine </h3>
+              <p> A Graphical User Interface that simulates the operation of a real-world vending machine where users can select and purchase fruits. </p>
+          </div>
+          <div className="card_concept_works_lower"> 
+            <button>
+              View Code <FontAwesomeIcon className="icon_fontawesome" icon={faArrowRight} />
+            </button>
+            <div className="card_concept_works_footer">
+                <p> Technologies used: </p>
+                <img src={pyIcon} className="icon_logos_technologies" alt="icon_figma" />
+            </div>
+          </div>
+          </div>  
+      </div>              
+         
       </div>
     </>
   );
